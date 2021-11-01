7bc0ce8e9c2f70b355986e9fc031bd9b42cac7cf
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2021-11-01 21:21:24 +0100

    Menu created and working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Avatar from './components/Avatar'
 import data from './data.js'
-
+import Menu from './components/Menu';
 
 class App extends React.Component {
   constructor()
@@ -11,15 +11,25 @@ class App extends React.Component {
       
       avatarWidth: "300px",
       avatarHeight: "600px",
-      avatarSrc: data.avatar
-
-
+      avatarSrc: data.avatar,
+      fullNameMenu: data.fullName,
+      experienceMenu: data.experienceM,
+      projectsMenu : data.projectsM,
+      abilitiesMenu : data.abilitiesM,
+      studiesMenu : data.studiesM
     } 
   }
 
   render(){
   return (
     <div className="App">
+    <Menu 
+    fullName = {this.state.fullNameMenu}
+    experienceM = {this.state.experienceMenu}
+    projectsM = {this.state.projectsMenu}
+    abilitiesM = {this.state.abilitiesMenu}
+    studiesM = {this.state.studiesMenu}
+    />
     <Avatar
     width={this.state.avatarWidth}
     height={this.state.avatarHeight}
--- src/data.js
+++ src/data.js
@@ -2,6 +2,11 @@ export default
 {
     "name": "Aitor",
     "surname": "Palleja",
+    "fullName": "Aitor Palleja",
     "phone": "606241920",
-    "avatar": "https://www.bootdey.com/img/Content/avatar/avatar7.png"
+    "avatar": "https://www.bootdey.com/img/Content/avatar/avatar7.png",
+    "projectsM": "Proyectos",
+    "studiesM": "Estudios",
+    "experienceM": "Experiencia laboral",
+    "abilitiesM": "Habilidades"
 };
